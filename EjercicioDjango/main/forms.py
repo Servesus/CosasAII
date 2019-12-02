ed2871029dbd4cbcc659e18337f438d4aeaef7a3
Servesus <jesusgarrocho1998@gmail.com>
2019-12-02 18:18:48 +0100

    a

--- EjercicioDjango/main/forms.py
+++ EjercicioDjango/main/forms.py
@@ -4,5 +4,5 @@ from django import forms
 class EventosFechaForm(forms.Form):
     fecha = forms.DateField(label="Fecha del evento", widget=forms.TextInput, required=True)
     
-class PeliculaBusquedaYearForm(forms.Form):
+class EventosIdiomaForm(forms.Form):
     idioma = forms.CharField(label="Idioma del evento", widget=forms.TextInput, required=True)
