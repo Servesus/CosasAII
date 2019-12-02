27b60d602d52d4d80fad7932da57debbb875af03
Servesus <jesusgarrocho1998@gmail.com>
2019-12-02 19:15:22 +0100

    a

--- EjercicioDjango/main/views.py
+++ EjercicioDjango/main/views.py
@@ -25,9 +25,10 @@ def populateDatabase(request):
     populateMunicipios()
     populateTipoEvento()
     populateIdioma()
-    with open('dataset-B.csv') as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=';')
-        for i in range(1, len(csv_reader)):
+    with open(path+'\\dataset-B.csv') as csv_file:
+        csv_reader = list(csv.reader(csv_file, delimiter=';'))
+        
+        for i in range(1,len(csv_reader)):
             Evento.objects.create(eventoId = i-1, nombre = csv_reader[i][0],
             tipo_evento = TipoEvento.objects.get(tipo_evento = csv_reader[i][1]),
             fecha_inicio = datetime.strptime(csv_reader[i][2], '%d/%m/%Y'),
@@ -38,21 +39,24 @@ def populateDatabase(request):
 
 def populateMunicipios():
     with open(path+'\\municipio.csv') as csv_file:
-        csv_reader = csv.reader(csv_file)
-        for i in range(1, len(csv_reader)):
+        csv_reader = list(csv.reader(csv_file))
+        row_count = sum(1 for row in csv_reader)
+        for i in range(1, row_count):
             Municipio.objects.create(municipioId = i-1, municipio = csv_reader[i])
-
+    print("1")
 def populateTipoEvento():
     with open(path+'\\tipoevento.csv') as csv_file:
-        csv_reader = csv.reader(csv_file)
+        csv_reader = list(csv.reader(csv_file))
         for i in range(1, len(csv_reader)):
-            TipoEvento.objects.create(tipoEventoId = i-1, tipo_evento = csv_reader[i])
+            TipoEvento.objects.create(tipo_evento = csv_reader[i])
+    print("2")
 
 def populateIdioma():
     with open(path+'\\lenguas.csv') as csv_file:
-        csv_reader = csv.reader(csv_file)
+        csv_reader = list(csv.reader(csv_file))
         for i in range(1, len(csv_reader)):
             Idioma.objects.create(idiomaId = i-1, idioma = csv_reader[i])
+    print("3")
 
 def mostrar_eventos(request):
     eventos = Evento.objects.all().order_by("municipio")
