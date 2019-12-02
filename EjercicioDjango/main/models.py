593ff883b348cd0386f9ab24e4e2b03700fe2dba
MrGaRRos <juanmagarrocho@gmail.com>
2019-12-02 18:22:57 +0100

    Modelo

--- EjercicioDjango/main/models.py
+++ EjercicioDjango/main/models.py
@@ -3,61 +3,65 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 
-class Ocupacion(models.Model):
-    ocupacionId = models.AutoField(primary_key=True)
-    nombre = models.TextField(verbose_name='Ocupación', unique=True)
+class Idioma(models.Model):
+    idiomaId = models.TextField(primary_key=True)
+    idioma = models.TextField(verbose_name= 'Idioma', help_text="Idioma")
 
+    def __str__(self):
+        return self.idioma
+    
+    class Meta:
+        ordering = ('idioma', )
+
+class Municipio(models.Model):
+    municipioId = models.TextField(primary_key=True)
+    municipio = models.TextField(verbose_name= 'Municipio', help_text="Municipio")
+
+    def __str__(self):
+        return self.municipio
+    
+    class Meta:
+        ordering = ('municipio', )     
+
+class TipoEvento(models.Model):
+    
+     TIPOS_EVENTOS =  ['Teatro',
+    'Proyeccion audiovisual',
+    'Exposicion',
+    'Actividad infantil',
+    'Concierto',
+    'otros',
+    'Concurso',
+    'Conferencia',
+    'Danza',
+    'Curso-taller',
+    'Festivales (Gazteinformazioa)',
+    'Jornada',
+    'Bertsolarismo',
+    'Presentacion',
+    'Feria']
+    
+    tipoEventoId = models.TextField(primary_key=True)
+    tipo_evento = models.TextField(verbose_name='Tipo de Evento', choices=TIPOS_EVENTOS)
+
+    def __str__(self):
+        return self.tipo_evento
+    
+    class Meta:
+        ordering = ('tipo_evento', )      
+
+class Evento(models.Model):
+    eventoId = models.AutoField(primary_key=True)
+    nombre = models.TextField(verbose_name='Evento', help_text= "Nombre del evento")
+    fecha_inicio = models.DateField(verbose_name='Fecha de Inicio', help_text = "Fecha de inicio ")
+    fecha_fin = models.DateField(verbose_name='Fecha de Finalización ', help_text = "Fecha de finalización")
+    precio = models.DecimalField(verbose_name= 'Precio', help_text= "Precio" )
+    municipio = models.ForeignKey(Municipio, on_delete=models.CASCADE)
+    tipo_evento = models.ForeignKey(TipoEvento, on_delete= models.CASCADE)
+    idiomas = models.ManyToManyField(Idioma)
     def __str__(self):
         return self.nombre
     
     class Meta:
-        ordering = ('nombre', )
+        ordering = ('fecha_inicio', )
 
-class Usuario(models.Model):
-    idUsuario = models.TextField(primary_key=True)
-    edad = models.IntegerField(verbose_name='Edad', help_text='Debe introducir una edad')
-    sexo = models.CharField(max_length=1, verbose_name='Sexo', help_text='Debe elegir entre M o F')
-    ocupacion = models.ForeignKey(Ocupacion, on_delete=models.SET_NULL, null=True)
-    codigoPostal = models.TextField(verbose_name='Código Postal')
-
-    def __str__(self):
-        return self.idUsuario
-    
-    class Meta:
-        ordering = ('idUsuario', )
-
-class Categoria(models.Model):
-    idCategoria = models.TextField(primary_key=True)
-    nombre = models.TextField(verbose_name='Categoría')
-
-    def __str__(self):
-        return self.nombre
-    
-    class Meta:
-        ordering =('nombre', )
-
-class Pelicula(models.Model):
-    idPelicula = models.TextField(primary_key=True)
-    titulo = models.TextField(verbose_name='Título')
-    fechaEstreno = models.DateField(verbose_name='Fecha de Estreno', null=True)
-    imdbUrl = models.URLField(verbose_name='URL en IMDB')
-    categorias = models.ManyToManyField(Categoria)
-    puntuaciones = models.ManyToManyField(Usuario, through='Puntuacion')
-
-    def __str__(self):
-        return self.titulo
-    
-    class Meta:
-        ordering = ('titulo', 'fechaEstreno', )
-
-class Puntuacion(models.Model):
-    PUNTUACIONES = ((1, 'Muy mala'), (2,'Mala'), (3,'Regular'), (4,'Buena'), (5,'Muy Buena'))
-    idUsuario = models.ForeignKey(Usuario,on_delete=models.CASCADE)
-    idPelicula = models.ForeignKey(Pelicula,on_delete=models.CASCADE)
-    puntuacion = models.IntegerField(verbose_name='Puntuación', validators=[MinValueValidator(0), MaxValueValidator(5)], choices=PUNTUACIONES)
-    
-    def __str__(self):
-        return (str(self.puntuacion))
-    
-    class Meta:
-        ordering=('idPelicula','idUsuario', )
