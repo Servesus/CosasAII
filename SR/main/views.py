f28161edde9c182e50b9c839a8fdce8bda82a9c0
ManuErCaximba <ercaximbero96@gmail.com>
2020-01-13 17:42:31 +0100

    ss

--- SR/main/views.py
+++ SR/main/views.py
@@ -1,6 +1,6 @@
 import shelve
 from main.models import Libro, Puntuacion
-from main.forms import UserForm, FilmForm
+from main.forms import UserForm, BookForm
 from django.shortcuts import render, get_object_or_404
 from main.recommendations import  transformPrefs, calculateSimilarItems, getRecommendations, getRecommendedItems, topMatches
 from main.populate import populateDatabase
@@ -11,11 +11,11 @@ from main.populate import populateDatabase
 def loadDict():
     Prefs={}   # matriz de usuarios y puntuaciones a cada a items
     shelf = shelve.open("dataRS.dat")
-    ratings = Rating.objects.all()
+    ratings = Puntuacion.objects.all()
     for ra in ratings:
-        user = int(ra.user.id)
-        itemid = int(ra.film.id)
-        rating = float(ra.rating)
+        user = int(ra.idUsuario)
+        itemid = int(ra.book)
+        rating = float(ra.puntuacion)
         Prefs.setdefault(user, {})
         Prefs[user][itemid] = rating
     shelf['Prefs']=Prefs
@@ -39,28 +39,29 @@ def loadRS(request):
     loadDict()
     return render(request,'loadRS.html')
  
-# APARTADO A
-def recommendedFilmsUser(request):
+# APARTADO D
+def recommendedBooksUser(request):
     if request.method=='GET':
         form = UserForm(request.GET, request.FILES)
         if form.is_valid():
             idUser = form.cleaned_data['id']
-            user = get_object_or_404(UserInformation, pk=idUser)
+            #user = get_object_or_404(UserInformation, pk=idUser)
             shelf = shelve.open("dataRS.dat")
             Prefs = shelf['Prefs']
             shelf.close()
             rankings = getRecommendations(Prefs,int(idUser))
             recommended = rankings[:2]
-            films = []
+            books = []
             scores = []
             for re in recommended:
-                films.append(Film.objects.get(pk=re[1]))
+                books.append(Book.objects.get(pk=re[1]))
                 scores.append(re[0])
-            items= zip(films,scores)
-            return render(request,'recommendationItems.html', {'user': user, 'items': items})
+            items= zip(books,scores)
+            return render(request,'recommendationItems.html', {'idUsuario': idUser, 'items': items})
     form = UserForm()
     return render(request,'search_user.html', {'form': form})
 
+"""
 # APARTADO B
 def recommendedFilmsItems(request):
     if request.method=='GET':
@@ -83,29 +84,31 @@ def recommendedFilmsItems(request):
             return render(request,'recommendationItems.html', {'user': user, 'items': items})
     form = UserForm()
     return render(request,'search_user.html', {'form': form})
+"""
 
 # APARTADO C
-def similarFilms(request):
-    film = None
+def similarBooks(request):
+    book = None
     if request.method=='GET':
-        form = FilmForm(request.GET, request.FILES)
+        form = BookForm(request.GET, request.FILES)
         if form.is_valid():
-            idFilm = form.cleaned_data['id']
-            film = get_object_or_404(Film, pk=idFilm)
+            idBook = form.cleaned_data['id']
+            book = get_object_or_404(Book, pk=idBook)
             shelf = shelve.open("dataRS.dat")
             ItemsPrefs = shelf['ItemsPrefs']
             shelf.close()
-            recommended = topMatches(ItemsPrefs, int(idFilm),n=3)
-            films = []
+            recommended = topMatches(ItemsPrefs, int(idBook),n=2)
+            books = []
             similar = []
             for re in recommended:
-                films.append(Film.objects.get(pk=re[1]))
+                books.append(Book.objects.get(pk=re[1]))
                 similar.append(re[0])
-            items= zip(films,similar)
-            return render(request,'similarFilms.html', {'film': film,'films': items})
-    form = FilmForm()
-    return render(request,'search_film.html', {'form': form})
+            items= zip(books,similar)
+            return render(request,'similarBooks.html', {'book': book,'books': items})
+    form = BookForm()
+    return render(request,'search_book.html', {'form': form})
 
+"""
 # APARTADO D
 def recommendedUsersFilms(request):
     if request.method=='GET':
@@ -127,13 +130,19 @@ def recommendedUsersFilms(request):
             return render(request,'recommendationUsers.html', {'film': film, 'items': items})
     form = FilmForm()
     return render(request,'search_film.html', {'form': form})
-#APARTADO E
+"""
+
+#APARTADO A
 def search(request):
     if request.method=='GET':
         form = UserForm(request.GET, request.FILES)
         if form.is_valid():
             idUser = form.cleaned_data['id']
-            user = get_object_or_404(UserInformation, pk=idUser)
-            return render(request,'ratedFilms.html', {'usuario':user})
+            #user = get_object_or_404(UserInformation, pk=idUser)
+            booksVotedByUser = Puntuacion.objects.all().filter(idUsuario=idUser)
+            books = []
+            for ra in booksVotedByUser:
+                books.append(Book.objects.get(idBook=ra.book.id))
+            return render(request,'ratedBooks.html', {'idUsuario':idUser, 'books':books})
     form=UserForm()
     return render(request,'search_user.html', {'form':form })
