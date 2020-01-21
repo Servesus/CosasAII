e13d6f8efe15e7a2081ccb87c4d9e4625fe1f50e
Servesus <jesusgarrocho1998@gmail.com>
2020-01-21 17:50:01 +0100

    Scraping g2a

--- Scraping/examenJesus.py
+++ Scraping/examenJesus.py
@@ -1,9 +1,12 @@
-from bs4 import BeautifulSoup
-import urllib.request
+from bs4 import BeautifulSoup,NavigableString
+from urllib.request import urlopen,Request
 from tkinter import *
 from tkinter import messagebox
 import sqlite3
+import re
 
+
+"""
 def extraer():
     nombres = []
     marcas = []
@@ -47,5 +50,42 @@ def extraer():
         
 
 extraer()
+"""
 
+def g2a(busqueda):
+    nombres = []
+    links = []
+    precios = []
+    imagenes = []
+    site = "https://www.g2a.com/search?query="+busqueda
+    hdr = {"authority": "www.g2a.com",
+            "method": "GET",
+            "path": "/search?query=gta",
+            "scheme": "https",
+            "accept-language": "es-ES,es;q=0.9,en;q=0.8",
+            "referer": "https://www.g2a.com",
+            "sec-fetch-mode": "navigate",
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36"
+    }
+    req = Request(site,headers=hdr)
+    page = urlopen(req)
+    soup = BeautifulSoup(page,"lxml")
+    juegos = soup.find("ul",class_="products-grid")
+    for juego in juegos:
+        card_media = juego.find("div",class_="Card__media")
+        link = "https://www.g2a.com" + card_media.a.get("href")
+        try:
+            imagen = card_media.a.find("img",class_="lazy-image__img Card__picture__img").get("data-src")
+        except:
+            imagen = ""
+        nombre = juego.find("h3",class_="Card__title").a.string
+        tags_precio = juego.find("div",class_="Card__body").find("div",class_="Card__footer").find("div",class_="Card__price").find("span",class_="Card__price-cost price")
+        precio = str(tags_precio).split(">")[1].split("<")[0]
+        nombres.append(nombre)
+        links.append(link)
+        precios.append(precio)
+        imagenes.append(imagenes)
+        return nombres,links,precios,imagenes
+        
 
+g2a("gta")
